3a59345faacd633466e605dd89190a982ade5bfd
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-23 08:03:30 -0400

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :subscriptions
     has_many :games, through: :subscriptions
-    has_secure_password
     validates :user_name, :first_name, :last_name, :email, :password, presence: true
     validates :user_name, :email, uniqueness: true
+    has_secure_password
 end

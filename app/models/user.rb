6e4bbc564ffd98399c2be0f4798403568e4abda0
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-23 09:30:57 -0400

    uncommented validations for users model; added form validation error messages for user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :subscriptions
     has_many :games, through: :subscriptions
-    # validates :user_name, :first_name, :last_name, :email, :password, presence: true
-    # validates :user_name, :email, uniqueness: true
+    validates :user_name, :first_name, :last_name, :email, :password, presence: true
+    validates :user_name, :email, uniqueness: true
     has_secure_password
 end

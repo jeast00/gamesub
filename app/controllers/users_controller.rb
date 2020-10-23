d588457fab52bbf9462f9f4f659490cc7f96df7a
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-23 09:12:18 -0400

    moved show method above new in users controller; changed users_path to @user for form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
     def create
         @user = User.new(user_params)
 
@@ -15,8 +19,23 @@ class UsersController < ApplicationController
         end
     end
 
-    def show
+    def edit
         @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_url
     end
 
     private

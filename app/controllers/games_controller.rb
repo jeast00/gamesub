0712ca20bac66a130e80fa38cb7138f5a6dbcccb
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-23 09:34:54 -0400

    added methods to games controller
    added CRUD methods for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,32 @@
 class GamesController < ApplicationController
+
+    def new
+        @game = Game.new
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :platform)
+    end
 end

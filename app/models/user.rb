02bd78ea382c27c91c4ff313032df9823f82183d
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-24 00:47:28 -0400

    Added class method for users model
    added an omniauth user class method for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
     validates :user_name, :first_name, :last_name, :email, :password, presence: true
     validates :user_name, :email, uniqueness: true
     has_secure_password
+
+    def self.find_or_create_by_omniauth(auth_hash) 
+        self.where(:user_name => auth_hash['info'][:user_name]).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
 end

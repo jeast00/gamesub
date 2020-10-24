e1f1bb6332de90663709f362e72b2412cbdc9749
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-24 00:41:29 -0400

    added omniauth logic to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,20 @@ class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def new
-        @user = User.new
     end
 
     def create
-         @user = User.find_by(user_name: params[:user_name])
-         if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        if auth_hash = request.env['omniauth.auth']
+            user = User.find_or_create_by_omniauth(auth_hash)
+            session[:user_id] = user.id
         else
-            redirect_to login_path 
+            user = User.find_by(user_name: params[:user_name])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect_to user_path
+            else
+                redirect_to login_path 
+            end
         end
     end
 
